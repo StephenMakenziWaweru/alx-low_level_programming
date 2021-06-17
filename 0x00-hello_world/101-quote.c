6bb3dca302f14a9c56c01c77b4ca2978ff729526
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-17 19:49:23 +0300

    stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+/*
+ * File 101-quote.c
+ * Auth: Stephen Makenzi Waweru <stevomakenzi@gmail.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * to the stderr followed by a new line.
+ *
+ *Return:Always 1.
+ */
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n`");
+
+	return (0);
+}
