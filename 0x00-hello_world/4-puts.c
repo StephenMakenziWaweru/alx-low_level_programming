5dcdd3e5e2aaf64fe8b4738445ff1060f244e874
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-17 14:19:33 +0000

    removed whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,7 @@
 
 /*
  * main - Prints "programming is like building a multilingual puzzle,
- * 		  folled by a new line.
- * 
+ * 		   folled by a new line.
  * Return: Always 0.
  */
 
@@ -16,5 +15,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
