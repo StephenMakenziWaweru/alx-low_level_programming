901b7e36023488a97350a0ac50c121e6d35de399
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-17 14:02:43 +0000

    added blank lines

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-void main()
+
+int main(void)
 {
-	puts("programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"programming is like building a multilingual puzzle");
+
+	return 0
 }
