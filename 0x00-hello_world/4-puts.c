7d0d046c35a89c6d1d9c6a2f44a9d6b0389d5e95
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-17 14:14:48 +0000

    added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,16 @@
+/*
+ * File: 4-puts.c
+ * Auth: Stephen Waweru <stevomakenzi@gmail.com>
+ */
+
 #include <stdio.h>
+
+/*
+ * main - Prints "programming is like building a multilingual puzzle,
+ * 		  folled by a new line.
+ * 
+ * Return: Always 0.
+ */
 
 int main(void)
 {
