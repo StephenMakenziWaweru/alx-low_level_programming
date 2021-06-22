4cc8d9fac22771334fd6e845132b94685673d486
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-22 16:36:16 +0300

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _abs - computes the absolute value of an integer
+ *@int: is the integer whose abs value is to be calculated.
+ * Return: abs value
+ */
+
+int _abs(int c)
+{
+	return (abs(c));
+}
+
