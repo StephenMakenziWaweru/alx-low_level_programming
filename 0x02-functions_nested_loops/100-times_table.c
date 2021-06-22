dd5dd3c3adb02c67b692cfc7d8010e3649acde8c
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-22 22:11:12 +0300

    Changed 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,55 +1,51 @@
 #include "holberton.h"
 
 /**
- * print_times_table - prints the n times table starting from 0.
- * @n: is the input value
+ * print_times_table - prints the times table up to the integer input
+ * @n: integer input
+ *
+ * Return: void
  */
+
 void print_times_table(int n)
 {
-	if (n <= 15 && n >= 0)
+	int a, b, c, d, e, f;
+
+	if (n >= 0 && n < 15)
 	{
-		int column = 0;
-
-		while (column <= n)
+		for (a = 0; a <=  n; a++)
 		{
-			int row = 0;
-
-			while (row <= n)
+			for (b = 0; b <= n; b++)
 			{
-				int result = row * column;
-
-				if (row == 0)
-					_putchar(result + '0');
-
-				else if (result <= 9)
+				c = a * b; d = c / 100; e = (c /  10) % 10;
+				f = (c % 100) % 10;
+				if (b == 0)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(result + '0');
+					_putchar('0');
 				}
-				else if (result <= 99)
+				else if (c < 10)
+				{
+					_putchar(' '); _putchar(' ');
+					_putchar('0' + f);
+				}
+				else if (c < 100)
 				{
 					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
+					_putchar('0' + e); _putchar('0' + f);
 				}
 				else
 				{
-					_putchar((result / 100) + '0');
-					_putchar(((result / 10) % 10) + '0');
-					_putchar((result % 10) + '0');
+					_putchar('0' + d);
+					_putchar('0' + e);
+					_putchar('0' + f);
 				}
-
-				if (row < n)
+				if (b < n)
 				{
-				_putchar(',');
-				_putchar(' ');
+					_putchar(','); _putchar(' ');
 				}
-
-				row++;
+				else
+					_putchar('\n');
 			}
-			_putchar('\n');
-			column++;
 		}
 	}
 }
