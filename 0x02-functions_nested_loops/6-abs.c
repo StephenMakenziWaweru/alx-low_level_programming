1ce2fb52c85a3f12db230f60ff87658ef0871e0a
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-22 16:38:11 +0300

    change int to c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 
 /**
  * _abs - computes the absolute value of an integer
- *@int: is the integer whose abs value is to be calculated.
+ *@c: is the integer whose abs value is to be calculated.
  * Return: abs value
  */
 
