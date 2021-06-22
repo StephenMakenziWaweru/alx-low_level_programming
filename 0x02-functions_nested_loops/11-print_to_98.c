52785bbe768fff3131a00e241628111d01afa493
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-22 20:05:23 +0300

    removed white space

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,10 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			printf("%d, ", n);
+			if (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
 			n++;
 		}
 	}
@@ -20,7 +23,10 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			printf("%d, ", n);
+			if  (n != 98)
+				printf("%d, ", n);
+			else
+				printf("%d", n);
 			n--;
 		}
 	}
