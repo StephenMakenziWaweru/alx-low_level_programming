fd574580f6a8836bd81ab442c0cb569bca477be6
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-22 14:56:10 +0300

    islower checker

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,17 @@
-/**
- * main - Prints lowercase alphabet letters followed by a new line
- *
- * Return: Always 0.
- */
+#include "holberton.h"
+#include <ctype.h>
 
+/**
+ *_islower - checks lowercase letters
+ *@c: character to check if it is lowercase
+ * Return: 1 if true and 0 if otherwise
+ */
+int _islower(int c)
+{
+	int k = islower(c);
+
+	if (k != 0)
+		return (1);
+	else
+		return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -3,4 +3,5 @@
 extern int _putchar(char i);
 extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
+extern int _islower(int c);
 #endif
