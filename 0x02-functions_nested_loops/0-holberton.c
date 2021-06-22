84c3248d35870952d8f8726702b4e001dcce96c3
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-22 10:33:41 +0300

    print holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Stephen Waweru
+ */
+
+#include <stdio.h>
+
+/*
+ * main - Prints "Holberton" followed by a new line
+ *
+ * Return: Always 0. 
+ */
+int main(void)
+{
+	printf("Holberton\n");
+
+	return (0);
+}
+
