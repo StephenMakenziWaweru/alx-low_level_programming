c1d4439d2801162607c4d482622da9af33663f41
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-24 16:03:58 +0300

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - draws a triangle in the terminal
+ * @size: is the size of triangle
+ */
+void print_triangle(int size)
+{
+	int i = 0;
+
+	if (size > 0)
+	{
+		while (i < size)
+		{
+			int j = size - 1;
+
+			while (j > i)
+			{
+				_putchar(' ');
+				j--;
+			}
+
+			int k = i + 1;
+
+			while (k > 0)
+			{
+				_putchar('#');
+				k--;
+			}
+
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+		_putchar('\n');
+}
