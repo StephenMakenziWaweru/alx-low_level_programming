766770f70999434e80db98e6ba9c0d4267bfbca2
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-28 19:48:11 +0300

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string followed by a new line
+ *
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	unsigned long int i;
+	unsigned long int n = strlen(str);
+
+	for (i = (n - 1) / 2; i < n; i++)
+	{
+		_putchar(*(str + i));
+	}
+
+	_putchar('\n');
+}
