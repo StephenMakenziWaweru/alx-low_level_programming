301d4c9aaf518969675617e3baa132a1a6c44c29
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-28 17:56:39 +0300

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string whose length is to be returned
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -3,4 +3,6 @@
 extern int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+#include <string.h>
 #endif
