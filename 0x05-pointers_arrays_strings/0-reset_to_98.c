411c50c6c2572a0034a37b312a86aa2ae78a380a
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-28 16:56:57 +0300

    holberton.h change pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - updates the value pointed to by a pointer to 98.
+ * @*n - pointer being updated.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+extern int _putchar(char c);
+
+#endif
