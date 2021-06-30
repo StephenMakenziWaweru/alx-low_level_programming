b2b204e9f594d2f9c5315edc90cc61e283503d1a
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-06-30 16:25:19 +0300

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: string to be encoded.
+ *
+ * Return: encoded string.
+ */
+
+char *rot13(char *s)
+{
+	int i = 0;
+	char letters[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13code[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+
+	while (s[i] != '\0')
+	{
+		int j = 0;
+
+		while (j < 52)
+		{
+			if (s[i] == letters[j])
+			{
+				s[i] = rot13code[j];
+
+				break;
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (s);
+}
