7c56c3bbd2233b40063259cb58e1cdbcb477da37
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-01 10:28:28 +0300

    changed from recursion to while

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,13 +7,26 @@
 
 void print_number(int n)
 {
+	int i = 0;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n / 10)
-		print_number(n / 10);
 
-	_putchar((n % 10) + '0');
+	if (n == 0)
+		_putchar('0');
+
+	while (n != 0)
+	{
+		i = i * 10;
+		i = i + n % 10;
+		n = n / 10;
+	}
+	while (i != 0)
+	{
+		_putchar((i % 10) + '0');
+		i = i / 10;
+	}
 }
