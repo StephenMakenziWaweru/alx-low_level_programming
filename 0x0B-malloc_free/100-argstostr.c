dd38921865d2f7d774bc72e774b461ef107dab1f
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-13 22:11:45 +0300

    another one

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,45 @@
 #include "holberton.h"
 #include <stdlib.h>
-
-
 /**
- *argstostr - concatenates all arguments of the program
- *@ac: integer
- *@av: character
- *
- *Return: pointer
+ * str_l - returns string length
+ * @s: string
+ * Return: string length
  */
+int str_l(char *s)
+{
+	int x = 0;
 
+	while (s[x] != '\0')
+		x++;
+	return (x);
+}
+/**
+ * argstostr - concats arguments of program
+ * @ac: takes arguments
+ * @av: array of arguments
+ * Return: pointer to new string
+ **/
 char *argstostr(int ac, char **av)
 {
-char *c;
-int i, j, size = 0, k = 0;
+	char *str;
+	int y, x, j;
+	int all = 0;
 
-if (ac == 0 || av == 0)
-return (NULL);
 
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-{
-size++;
-}
-size++;
-}
-size++;
-
-c = malloc(size);
-
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j]; j++)
-{
-c[k] = av[i][j];
-k++;
-}
-c[k] = '\n';
-k++;
-}
-c[k] = '\0';
-return (c);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (y = 0; y < ac; y++)
+		all += str_l(av[y]) + 1;
+	str = malloc(sizeof(char) * all + 1);
+	if (str == NULL)
+		return (NULL);
+	for (y = 0, j = 0; y < ac; y++)
+	{
+		for (x = 0; av[y][x] != '\0'; x++, j++)
+			str[j] = av[y][x];
+		str[j] = '\n';
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
 }
