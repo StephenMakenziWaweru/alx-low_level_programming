d7abcd50fb6317e9c193961926e168d35f4b7dda
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-13 12:35:27 +0300

    pointer to some memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory, which
+ * contatins a copy of the string given as a parameter.
+ * @str: string to be copied.
+ *
+ * Return: pointer to space allocated.
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int len = 0;
+	
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (*str++)
+			len++;
+
+		p = malloc((sizeof(*str) * len) + 1);
+
+		if (p == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			while (len >= 0)
+			{
+				p[len] = *--str;
+				len--;
+			}
+		}
+	}
+	return (p);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -4,4 +4,5 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 #include <stdlib.h>
 #include <stddef.h>
+char *_strdup(char *str);
 #endif
