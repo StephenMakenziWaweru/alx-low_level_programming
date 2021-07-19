ce8dbd2e5bc00238e9c5bba3e9e9d95984939db6
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-19 15:12:04 +0300

    added some loops and NULL checks

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,20 +13,57 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *p;
-	
+	unsigned int name_len, owner_len, i = 0;
+	unsigned int _strlen(char *str);
+
 	if (!name || !owner)
 		return (NULL);
 
 	p = malloc(sizeof(dog_t));
 	if (!p)
 		return (NULL);
+	
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
+	
+	p->name = malloc(name_len * sizeof(char));
+	if (!(p->name))
+		return (free(p), NULL);
+	while (i < name_len)
+	{
+		p->name[i] = name[i];
+		i++;
+	}
 
-	p->name = name;
 	p->age = age;
-	p->owner = owner;
+
+	p->owner = malloc(owner_len * sizeof(char));
+	if (!(p->owner))
+		return (free(p->name), free(p), NULL);
+	i = 0;
+	while (i < owner_len)
+	{
+		p->owner[i] = owner[i];
+		i++;
+	}
 
 	return (p);
 }
 
+/**
+ * _strlen - returns length of a string.
+ * @str: string whose length is to be returned.
+ *
+ * Return: length of string.
+ */
 
+unsigned int _strlen(char *str)
+{
+	unsigned int i = 0;
+
+	while(*str++)
+		i++;
+
+	return (i);
+}
 
