02b598028666f85fb741faa38b678bc2ae740148
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-19 12:56:35 +0300

    structure'

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure containing dog attributes.
+ * @name: name of dog
+ * @age: dog's age.
+ * @owner: dog's owner.
+ *
+ * Description: attributes of a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
