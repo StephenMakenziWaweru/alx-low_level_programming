ebfca294750e3ca1eec7d03244c93cd60e32e432
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-21 13:33:38 +0300

    removed extra desc

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,6 @@
  * print_name - prints a name
  * @name: name to be printed
  * @f: pointer to function.
- * @char: name passed to that function.
  */
 
 void print_name(char *name, void (*f)(char *))
