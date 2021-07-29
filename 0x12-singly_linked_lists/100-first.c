72d51c1f2757e3b6cc16ba95ab37129fa496584b
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-29 16:52:37 +0300

    added a constructor func

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - executes before main.
+ */
+
+void first(void) __attribute__((constructor));
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
