b34d22877e9dcfcf29288193b0c2d5bed501a983
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-07-29 16:59:04 +0300

    split the statement in two

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,9 @@
  * first - executes before main.
  */
 
-void first(void) __attribute__((constructor));
-
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my");
+	printf(" back!\n");
 }
+void first(void) __attribute__((constructor));
