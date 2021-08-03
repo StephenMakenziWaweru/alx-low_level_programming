371b0f4935e474eee7d502162689107c836e88a5
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-08-03 10:37:40 +0300

    added a check for head Nullity

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,10 +13,11 @@ int pop_listint(listint_t **head)
 	listint_t *temp = *head;
 	int n;
 
-	n = temp->n;
-	if (!n || head == NULL)
+	if (*head == NULL || head == NULL)
 		return (0);
+	temp = *head;
 	*head = temp->next;
+	n = temp->n;
 	free(temp);
 
 	return (n);
