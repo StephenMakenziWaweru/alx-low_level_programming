37929b0975eb9029729ea3706017a8462d5cc039
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-08-05 14:05:03 +0300

    changed approach

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,51 +1,30 @@
 #include "main.h"
 
 /**
- * _pow - returns the value of a number raised to some power.
- * @base: base.
- * @power: raised to.
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
  *
- * Return: result of a number raised to a power.
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
-int _pow(int base, int power)
-{
-	int result = 1;
-
-	while (power)
-	{
-		result *= base;
-		power--;
-	}
-	return (result);
-}
-/**
- * binary_to_uint - converts biary no. to an unsigned int.
- * @b: pointer to binary no.
- *
- * Return: int or 0 on failure.
- */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0, j = 0, uint = 0;
+	int i;
+	unsigned int num;
 
+	num = 0;
 	if (!b)
 		return (0);
-	while (b[i] != '\0')
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] >= '0' && b[i] <= '1')
-			i++;
-		else
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-
-	while (b[j] != '\0')
+	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[j] >= '1' && b[j] <= '1')
-			uint += _pow(2, (i - 1));
-		i--;
-		j++;
+		num <<= 1;
+		if (b[i] == '1')
+			num += 1;
 	}
-
-	return (uint);
+	return (num);
 }
+
