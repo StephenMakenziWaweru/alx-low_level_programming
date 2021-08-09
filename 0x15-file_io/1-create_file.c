adf67b51a81d1e6304c504f08449b9487df07cfd
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-08-09 13:08:19 +0300

    check for nullity

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,16 +11,20 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, wr, len;
-
+	
+	if (!filename)
+		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
-	for (len = 0; text_content[len] != '\0'; len++)
-		;
-
-	wr = write(fd, text_content, len);
-	if (wr == -1)
-		return (-1);
+	if (text_content)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+		wr = write(fd, text_content, len);
+		if (wr == -1)
+			return (-1);
+	}
 	close(fd);
 
 	return (1);
