4547c5712c27a168c652dd8fadcbb4cd6e0607cf
StephenMakenziWaweru <stevomakenzi@gmail.com>
2021-08-09 18:51:44 +0300

    used macros to defin error msgs

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,7 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#define EXIT98 \
+	do { \
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", \
+		file_from); \
+		exit(98); \
+	} while (0)
+#define EXIT99 \
+	do { \
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", \
+		file_to); \
+		exit(99); \
+	} while (0)
+#define EXITF100 \
+	do { \
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);\
+		exit(100); \
+	} while (0)
+#define EXITT100 \
+	do { \
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);\
+		exit(100); \
+	} while (0)
 /**
  * _close - closes open fd.
  * @fd_from: 1st fd.
@@ -16,16 +37,10 @@ void _close(int fd_from, int fd_to)
 
 	cl_from = close(fd_from);
 	if (cl_from == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		EXITF100;
 	cl_to = close(fd_to);
 	if (cl_to == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+		EXITT100;
 }
 
 /**
@@ -35,44 +50,29 @@ void _close(int fd_from, int fd_to)
  */
 void _cp(const char *file_from, const char *file_to)
 {
-	int rd = 1024, wr, fd_from, fd_to, char buf[1024];
+	int rd = 1024, wr, fd_from, fd_to;
+	char buf[1024];
 
 	if (!file_from)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		EXIT98;
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		EXIT98;
+
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
+		EXIT99;
+
 	while (rd == 1024)
 	{
 		rd = read(fd_from, buf, 1024);
 		if (rd == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
+			EXIT98;
 		wr = write(fd_to, buf, rd);
 		if (wr == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
+			EXIT99;
 		if (wr != rd)
-		{
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
+			EXIT99;
 	}
 	_close(fd_from, fd_to);
 }
