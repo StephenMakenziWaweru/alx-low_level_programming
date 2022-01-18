942ed0ee7ae4607d324c4b8d9a4ae52028d7084e
StephenMakenziWaweru <stevomakenzi@gmail.com>
2022-01-18 13:58:34 +0300

    104

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,8 +13,8 @@ int binary_search(int *array, size_t size, int value)
 	size_t start = 0, end = size - 1, mid;
 	size_t i;
 
-	if (!array)
-		return (-1)
+	if (!array || !size)
+		return (-1);
 
 	while (start <= end)
 	{
